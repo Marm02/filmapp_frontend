55435e8354d085117a6e6d3986fc8b479b0a8ed5
Marm02 <omgjan@interia.pl>
2019-05-05 15:32:57 +0200

    Updates

---
+++ precache-manifest.2e25b4dcb6e9ed042499a2d3d28f7486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "280ed588460cab8b7bf9",
+    "url": "/filmapp_frontend/static/js/runtime~main.fbaaed63.js"
+  },
+  {
+    "revision": "2f4033f699d79ea2b195",
+    "url": "/filmapp_frontend/static/js/main.da46d39e.chunk.js"
+  },
+  {
+    "revision": "be67861482aed980e0c9",
+    "url": "/filmapp_frontend/static/js/2.7f07f3b4.chunk.js"
+  },
+  {
+    "revision": "2f4033f699d79ea2b195",
+    "url": "/filmapp_frontend/static/css/main.240b8bc6.chunk.css"
+  },
+  {
+    "revision": "be67861482aed980e0c9",
+    "url": "/filmapp_frontend/static/css/2.bb8bf0d6.chunk.css"
+  },
+  {
+    "revision": "7390719f9075eefe1b88516da0a7b4b2",
+    "url": "/filmapp_frontend/index.html"
+  }
+];
