86401b84dcc6504033d3065e6ea5023586674c91
Marm02 <omgjan@interia.pl>
2019-05-11 22:22:54 +0200

    Updates

---
+++ precache-manifest.084388c1e795e1f8af6c9d608fd11492.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5c2ef2326093908bc9828aa3684c062",
+    "url": "/filmapp_frontend/static/media/ico.f5c2ef23.png"
+  },
+  {
+    "revision": "edab0e1695f40bbacc18b092a1af8650",
+    "url": "/filmapp_frontend/static/media/error-404.edab0e16.png"
+  },
+  {
+    "revision": "280ed588460cab8b7bf9",
+    "url": "/filmapp_frontend/static/js/runtime~main.fbaaed63.js"
+  },
+  {
+    "revision": "98ea73eff852cf689b29",
+    "url": "/filmapp_frontend/static/js/main.2d222bcc.chunk.js"
+  },
+  {
+    "revision": "1b80ad69279d0d97f8fd",
+    "url": "/filmapp_frontend/static/js/2.45fea34b.chunk.js"
+  },
+  {
+    "revision": "98ea73eff852cf689b29",
+    "url": "/filmapp_frontend/static/css/main.d655704f.chunk.css"
+  },
+  {
+    "revision": "1b80ad69279d0d97f8fd",
+    "url": "/filmapp_frontend/static/css/2.b4ed14eb.chunk.css"
+  },
+  {
+    "revision": "dc0e7ddc824e6627e07eb7978d949728",
+    "url": "/filmapp_frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/filmapp_frontend/precache-manifest.049d775c3fb6da6c2b6ae7c35b85cc64.js"
+  "/filmapp_frontend/precache-manifest.084388c1e795e1f8af6c9d608fd11492.js"
 );
 
 workbox.clientsClaim();
