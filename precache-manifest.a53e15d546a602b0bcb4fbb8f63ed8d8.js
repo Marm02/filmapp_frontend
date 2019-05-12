0f0722b357e9e64c25d192ec1ea6b6216c1dbcb4
Marm02 <omgjan@interia.pl>
2019-05-12 14:57:51 +0200

    Updates

---
+++ precache-manifest.a53e15d546a602b0bcb4fbb8f63ed8d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5c2ef2326093908bc9828aa3684c062",
+    "url": "/filmapp_frontend/static/media/ico.f5c2ef23.png"
+  },
+  {
+    "revision": "edab0e1695f40bbacc18b092a1af8650",
+    "url": "/filmapp_frontend/static/media/error-404.edab0e16.png"
+  },
+  {
+    "revision": "280ed588460cab8b7bf9",
+    "url": "/filmapp_frontend/static/js/runtime~main.fbaaed63.js"
+  },
+  {
+    "revision": "571ef9b7735204e0f47c",
+    "url": "/filmapp_frontend/static/js/main.0e320aed.chunk.js"
+  },
+  {
+    "revision": "210b575be07dfb92a202",
+    "url": "/filmapp_frontend/static/js/2.5b16e790.chunk.js"
+  },
+  {
+    "revision": "571ef9b7735204e0f47c",
+    "url": "/filmapp_frontend/static/css/main.46b8b3f3.chunk.css"
+  },
+  {
+    "revision": "210b575be07dfb92a202",
+    "url": "/filmapp_frontend/static/css/2.bfa25b4c.chunk.css"
+  },
+  {
+    "revision": "8fd398887ae9ccfe408db8a9834895ac",
+    "url": "/filmapp_frontend/index.html"
+  }
+];
